a47c1888a38bd50f6952fe9e1eb24f828d639ccf
mokaba <momeh1413@gmail.com>
2019-08-16 07:07:57 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
-console.log("Up and running!");
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 
-cards = ["queen", "queen", "king", "king"];
+//cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
 function checkForMatch() {
@@ -12,9 +33,11 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
@@ -38,5 +61,5 @@ cardsInPlay.push(cardTwo);*/
 
 
 
-console.log(cardsInPlay);
-console.log("User flipped " + cardsInPlay);
+//console.log(cardsInPlay);
+//console.log("User flipped " + cardsInPlay);
