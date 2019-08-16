2dd085f433171662b9cb69b3102ade315ce30498
mokaba <momeh1413@gmail.com>
2019-08-16 00:26:20 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,23 +2,41 @@ console.log("Up and running!");
 
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-var cardOne;
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-
-
-var cardTwo;
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("you found a match!");
   } else {
     alert("Sorry, try again.");
   }
 }
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+/*var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);*/
+
+
+
+
 
 console.log(cardsInPlay);
 console.log("User flipped " + cardsInPlay);
