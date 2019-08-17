8cc2c4052d247c10b351d6a8b9ce95a2fd0236c6
mokaba <momeh1413@gmail.com>
2019-08-17 18:28:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,14 +25,16 @@ var cards = [
 cardsInPlay = [];
 
 function checkForMatch() {
+  document.getElementById('game-board').setAttribute('src', cards.cardImage);
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("you found a match!");
   } else {
     alert("Sorry, try again.");
-  }
-}
+  };
+};
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
@@ -41,11 +43,22 @@ function flipCard(cardId) {
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
-  }
-}
+  };
+};
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for(var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+    //cardElement.appendChild('game-board');
+  };
+
+};
+
+createBoard();
 
 
 
